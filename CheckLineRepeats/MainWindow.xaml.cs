1506838654e69c34eb0593510a1c1fd06ed0a27b
kot2202 <kot2202@onet.pl>
2022-03-10 16:55:27 +0100

    first working version but not very readable line count

--- CheckLineRepeats/MainWindow.xaml.cs
+++ CheckLineRepeats/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Linq;
 
 namespace CheckLineRepeats
 {
@@ -25,9 +15,43 @@ namespace CheckLineRepeats
             InitializeComponent();
         }
 
-        private void Button_Click()
+        private void ClearOutputBox()
         {
+            outputTextBox.Text = string.Empty;
+        }
 
+        private void OpenFile_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "test";
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = "Text documents (.txt)|*.txt";
+
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                ClearOutputBox();
+
+                string filename = dialog.FileName;
+                List<string> lines = new List<string>();
+
+                using (var sr = new StreamReader(filename))
+                {
+                    var readLine = sr.ReadLine();
+                    while (readLine != null)
+                    {
+                        lines.Add(readLine);
+                        readLine = sr.ReadLine();
+                    }
+                }
+                // list with line + it's count
+                var duplicates = lines.GroupBy(x => x).Where(g => g.Count() > 1).Select(y => new {Element = y.Key, Counter = y.Count()}).ToList();
+
+                for(int i = 0; i < duplicates.Count(); i++)
+                {
+                    outputTextBox.Text += duplicates[i].Element + "\t\t\t" + duplicates[i].Counter + "\n";
+                }
+            }
         }
     }
 }
